e9d2894ebddb0a2977b96aecf9be98abecd68c85
Daniel Schneider <daschne8@gmail.com>
2019-03-05 18:04:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -67,7 +67,7 @@ def player_numbers(team)
   game_hash.each do |team_location,team_data|
     if team_data[:team_name] == team
       team_data[:players].each do |player|
-        player.each do |player_name,player_stats|
+        player.each do |player_name|
           puts player_stats
           #numbers << player_stats[:number]
         end
